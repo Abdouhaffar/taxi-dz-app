750b411413f119f08129f5171ce89c25d171b393
Abdouhaffar <86650365+Abdouhaffar@users.noreply.github.com>
2026-05-27 16:52:16 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback } from "react";
+import { useState, use, useRef, useCallback } from "react";
 import { auth, db } from "./firebase";
 import {
   createUserWithEmailAndPassword, signInWithEmailAndPassword,
