9dfc5ce20c85bd4b46ebd15fa060bbe5af387849
Abdouhaffar <86650365+Abdouhaffar@users.noreply.github.com>
2026-07-08 12:57:19 +0100

    Update Admin.jsx

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,23 +1,7 @@
 import { useState, useEffect } from "react";
-import { initializeApp, getApps } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
-import { getFirestore, collection, onSnapshot, doc, updateDoc, deleteDoc, setDoc, serverTimestamp, query, orderBy } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
-
-let auth, db;
-try {
-  const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
-  auth = getAuth(app);
-  db = getFirestore(app);
-} catch (e) { console.log(e); }
+import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
+import { collection, onSnapshot, doc, updateDoc, deleteDoc, setDoc, serverTimestamp, query, orderBy, addDoc } from "firebase/firestore";
+import { auth, db } from "./firebase";
 
 const SUPER_ADMIN_EMAIL = "abdouhaffar@gmail.com";
 const SUPER_ADMIN_CODE = "123456";
