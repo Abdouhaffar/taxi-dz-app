88b8d244cb584286b3fb1984f85f94e0c076237a
Abdouhaffar <86650365+Abdouhaffar@users.noreply.github.com>
2026-07-17 09:15:34 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1412,4 +1412,4 @@ export default function App() {
 
   return (
     <div style={{ maxWidth:390,margin:"0 auto",minHeight:"100vh" }}>
-      <style>{`@import url('https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700;800;900&display=swap')
+      <style>{"@import url('https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700;800;900&display=swap')
