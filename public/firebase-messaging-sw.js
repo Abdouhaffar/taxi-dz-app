eb7c4e60f3f9e59193a1f0b7878eb0c1c79ae174
Abdouhaffar <86650365+Abdouhaffar@users.noreply.github.com>
2026-07-17 09:12:32 +0200

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,3 +1,6 @@
+// AL-BURAQ - Firebase Messaging Service Worker
+// ضعه في مجلد public/
+
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
