89db1a7cd8a91c9c36fbd5dc2ae361cafc96a501
Abdouhaffar <86650365+Abdouhaffar@users.noreply.github.com>
2026-09-06 23:32:59 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import AdminApp from './Admin';
+
+const isAdmin = window.location.pathname.startsWith('/admin');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {isAdmin ? <AdminApp /> : <App />}
   </React.StrictMode>
 );
